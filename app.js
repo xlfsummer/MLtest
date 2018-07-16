6c91151c08444b5804f4bdef459c1352bff3b693
xlf-summer <xlf-summer@qq.com>
2018-07-17 00:23:11 +0800

    优化代码

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-let { NNode, NLayer, NNetwork, cost } = require("./lib/NerveNetwork.js");
+let { NNode, NLayer, NNetwork, calCost, dσ } = require("./lib/NerveNetwork.js");
 let getPixel = require("get-pixels");
 let util = require('util');
+let fs = require('fs');
 
 
 main();
@@ -8,10 +9,72 @@ main();
 async function main() {
     let net = new NNetwork(400, 16, 16, 10);
 
-    let pixels = await util.promisify(getPixel)('./asset/1,0.png')
+    let allAssets = await util.promisify(fs.readdir)("./asset/");
+
+    while(allAssets.length){
+        let batchFileNames = allAssets.splice(0, 10);
+
+        let batchOpts = [];
+        for(let fileName of batchFileNames){
+            let expectNumber = fileName.match(',(\d)\.')[1];
+
+            let expectArr = new Array(10).fill(0);
+            expectArr[expectNumber] = 1;
+
+            let monoColorData = await readInput('./asset/' + fileName);
+            batchOpts.push({
+                input: monoColorData,
+                expect: expectArr
+            });
+        }
+        let gradient = singleBatch(net, batchOpts);
+    }
+}
+
+/**
+ * @param {NNetwork} net
+ * @param {{input: number[], expect: string[]}[10]}
+ */
+function singleBatch(net, opts){
+    for(let {input, expect} of opts){
+        net.calc(input);
+        net.expect(expect);
+    }
+}
+
+/**
+ * @param {string} src
+ */
+async function readInput(src){
+    let pixels = await util.promisify(getPixel)(src)
     /** @type {Uint8Array} */
     var data = pixels.data;
     var monoColorData = Array.from(data.filter((v, i) => !(i % 4))).map(v => v / 255);
-    net.calc(monoColorData);
-    let loss = cost(net, 0);
+    return monoColorData;
+}
+
+/** 反向传播 */
+/**
+ * @param {NLayer} layerI
+ * @param {NLayer} layerO
+ */
+function BP(layerI, layerO){
+    for(let nodeO of layerO.nodes){
+        let cost = (nodeO.a - layerO.expect) ** 2;
+        let E = layerO.expect;
+        let aO = nodeO.a;
+        let zO = nodeO.b;
+        for(let [nodeI, weight] of nodeO.backLinks){
+            zO += nodeI.a * weight;
+        }
+        nodeO.db = 2(aO - E) * dσ(zO) * 1;
+
+        nodeO.dw = [];
+        for(let i = 0, l = nodeO.backLinks.length; i < l; i ++){
+            let nodeI = nodeO.backLinks[i][0];
+            nodeO.dw[i] = 2(aO - E) * dσ(zO) * nodeI.a;
+        }
+
+
+    }
 }
--- generateImage.js
+++ generateImage.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const svg2png = require("svg2png");
 const util = require("util");
 
-const COUNT = 100;
+const COUNT = 10;
 
 let genId = 1;
 let opt = {
@@ -18,13 +18,15 @@ let opt = {
 main();
 
 async function main(){
+    if(!(await util.promisify(fs.exists)("./assets")))
+        await util.promisify(fs.mkdir)("./assets");
     for (let index = 0; index < COUNT; index++)
         await gen(genId++);
 }
 
 async function gen(genId) {
     let captcha = svgCaptcha.create(opt);
-    let svgFileName = `./asset/${genId},${captcha.text}.svg`;
+    let svgFileName = `./assets/${genId},${captcha.text}.svg`;
     await util.promisify(fs.writeFile)(svgFileName, captcha.data, "utf8");
     let f = await util.promisify(fs.readFile)(svgFileName);
     let pngBuffer = await svg2png(f);
